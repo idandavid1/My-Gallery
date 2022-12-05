2e6f87407547cd4cd6f7659d2457f2554b9daf99
Idan David <idand2000@gmail.com>
2022-12-05 18:26:29 +0200

    fix render and model

--- ca-gallery-master/js/main.js
+++ ca-gallery-master/js/main.js
@@ -1,21 +1,17 @@
 console.log('Starting up')
 console.log('date:',new Date('2022-12-4').toString('dddd MMM yyyy'))
 // console.log('');
+$(document).ready(onInit)
+
 function onInit(){
-    renderGallery()
+  renderPortfolio()
 }
 
-function renderGallery(){
-    renderPortFolio()
-    renderModal()
-}
-
-function renderPortFolio(){
+function renderPortfolio(){
   const projects = getProjects()
   const strHTMLs = projects.map(project => {
-  return `<div class="row">
-          <div class="col-md-4 col-sm-6 portfolio-item">
-            <a class="portfolio-link" data-toggle="modal" href="#${project.id}">
+  return ` <div class="col-md-4 col-sm-6 portfolio-item">
+            <a class="portfolio-link" data-toggle="modal" onclick="renderModal('${project.id}')" href="#portfolioModal">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fa fa-plus fa-3x"></i>
@@ -27,50 +23,31 @@ function renderPortFolio(){
               <h4>${project.name}</h4>
               <p class="text-muted">${project.title}</p>
             </div>
-          </div>`
+        </div> `
   })
   $('.portfolio-container').html(strHTMLs.join(''))
 }
 
-function renderModal(){
-    const projects = getProjects()
-    const strHTMLs = projects.map(project => {
-    return `<div class="portfolio-modal modal fade" id="${project.id}" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="close-modal" data-dismiss="modal">
-          <div class="lr">
-            <div class="rl"></div>
-          </div>
-        </div>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 mx-auto">
-              <div class="modal-body">
-                <h2>${project.name}</h2>
-                <p class="item-intro text-muted">${project.title}</p>
-                <!-- need to change -->
-                <img class="img-fluid d-block mx-auto" src="img/portfolio/01-full.jpg" alt="">
-                <p>${project.desc}</p>
-                <ul class="list-inline">
-                  <li>Date: ${myGetDate(project.publishedAt)}</li>
-                  <li>Category: ${project.labels.join(', ')}</li>
-                </ul>
-                <button class="btn btn-primary" data-dismiss="modal" type="button">
-                    <i class="fa fa-times"></i>
-                    Close Project</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>`
-  })
-  $('.modals-container').html(strHTMLs.join(''))
+function renderModal(projectId){
+    const project = getProjectById(projectId)
+    const strHTML =
+    `<h2>${project.name}</h2>
+    <p class="item-intro text-muted">${project.title}</p>
+    <img class="img-fluid d-block mx-auto" src="img/portfolio/01-full.jpg" alt="">
+    <p>${project.desc}</p>
+    <ul class="list-inline">
+      <li>Date: ${myGetDate(project.publishedAt)}</li>
+      <li>Category: ${project.labels.join(', ')}</li>
+    </ul>
+    <a class="btn btn-primary" target="_blank" rel="noopener" href="${project.url}">Open Project</a>
+    <button class="btn btn-primary" data-dismiss="modal" type="button">
+    <i class="fa fa-times"></i>
+    Close Project</button>`
+ 
+  $('.modal-body').html(strHTML)
 }
 
 function myGetDate(millisecondsDate){
   const date = new Date(millisecondsDate)
-  return `${date.getDay} / ${date.getDate + 1} / ${date.getFullYear}`
+  return `${date.getDay()} / ${date.getDate() + 1} / ${date.getFullYear()}`
 }
